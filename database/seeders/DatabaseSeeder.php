0bf63b4618093f226216c2c37f7c5372f1b9a206
Eric Liao <d1044171009@gmail.com>
2024-02-08 13:27:23 +0800

    feat: 修改種子資料功能加入任務時間

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Seeder;
 use App\Models\Task;
 
@@ -14,10 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // 建立五個用戶，每個用戶有十個任務
-        User::factory(5)->has(
-            // 建立十個任務
-            Task::factory(10)
-        )->create();
+        $start = now()->startOfMonth()->subMonthsNoOverflow();
+        $end = now();
+        $period = CarbonPeriod::create($start, '1 day', $end);
+
+        User::factory(5)->create()->each(function ($user) use($period) {
+            foreach ($period as $date) {
+                // 隨機時間修改小時
+                $date->hour(rand(0, 23))
+                    ->minute(rand(0, 6) * 10);
+                Task::factory()->create([
+                    'user_id' => $user->id,
+                    'created_at' => $date,
+                    'updated_at' => $date,
+                ]);
+            }
+        });
     }
 }
